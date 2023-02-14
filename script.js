cfc1c432a3418b06cce161d6e34fb75787e6594a
Wie Kiang H <wiekiang@gmail.com>
2023-02-14 13:27:13 +1000

    add new game event

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-'use strict';
+/** @format */
+
+"use strict";
+
+const btnNewGame = document.querySelector(".btn--new");
+
+const newGame = function () {
+    document.querySelector("#score--0").textContent = 0;
+    document.querySelector("#score--1").textContent = 0;
+    document.querySelector("#current--0").textContent = 0;
+    document.querySelector("#current--1").textContent = 0;
+};
+
+newGame();
+
+// New Game
+btnNewGame.addEventListener("click", newGame);
