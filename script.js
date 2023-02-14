ad4db3e059e9ef2c4d05ad87089b5c31a3491202
Wie Kiang H <wiekiang@gmail.com>
2023-02-14 13:35:17 +1000

    set starting player when new game clicked

--- script.js
+++ script.js
@@ -5,10 +5,21 @@
 const btnNewGame = document.querySelector(".btn--new");
 
 const newGame = function () {
+    // Reset score to 0
     document.querySelector("#score--0").textContent = 0;
     document.querySelector("#score--1").textContent = 0;
     document.querySelector("#current--0").textContent = 0;
     document.querySelector("#current--1").textContent = 0;
+
+    // Set player 1 as starting player
+    if (
+        !document
+            .querySelector(".player--0")
+            .classList.contains("player--active")
+    ) {
+        document.querySelector(".player--0").classList.toggle("player--active");
+        document.querySelector(".player--1").classList.toggle("player--active");
+    }
 };
 
 newGame();
