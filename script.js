10b028d447b6ba3c52fe230bcd85aaf1473e06bc
Wie Kiang H <wiekiang@gmail.com>
2023-02-14 17:24:41 +1000

    add hold score event

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ let activePlayer;
 
 const btnNewGame = document.querySelector(".btn--new");
 const btnRollDice = document.querySelector(".btn--roll");
+const btnHoldScore = document.querySelector(".btn--hold");
 
 const switchPlayer = function () {
     document.querySelector(".player--0").classList.toggle("player--active");
     document.querySelector(".player--1").classList.toggle("player--active");
+    activePlayer = activePlayer === 0 ? 1 : 0;
 };
 
 const newGame = function () {
@@ -19,6 +21,9 @@ const newGame = function () {
     document.querySelector("#score--1").textContent = 0;
     document.querySelector("#current--0").textContent = 0;
     document.querySelector("#current--1").textContent = 0;
+
+    btnRollDice.style.display = "block";
+    btnHoldScore.style.display = "block";
 
     // Set player 1 as starting player
     if (
@@ -52,12 +57,31 @@ btnRollDice.addEventListener("click", function () {
     // Display score
     if (randomDice === 1) {
         document.querySelector(`#current--${activePlayer}`).textContent = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
         switchPlayer();
     } else {
         document.querySelector(`#current--${activePlayer}`).textContent =
             Number(
                 document.querySelector(`#current--${activePlayer}`).textContent
             ) + randomDice;
+    }
+});
+
+// Hold score
+btnHoldScore.addEventListener("click", function () {
+    // Add current score to total
+    document.querySelector(`#score--${activePlayer}`).textContent =
+        Number(document.querySelector(`#score--${activePlayer}`).textContent) +
+        Number(document.querySelector(`#current--${activePlayer}`).textContent);
+
+    if (
+        Number(document.querySelector(`#score--${activePlayer}`).textContent) >=
+        10
+    ) {
+        btnRollDice.style.display = "none";
+        btnHoldScore.style.display = "none";
+    } else {
+        // Set current score to 0 and switch player
+        document.querySelector(`#current--${activePlayer}`).textContent = 0;
+        switchPlayer();
     }
 });
